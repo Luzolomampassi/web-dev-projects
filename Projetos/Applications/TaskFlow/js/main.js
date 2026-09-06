a47e29bc558089653c16d4291cf54407ba69ef3a
Luzolo mampassi <luzolomampassi8@gmail.com>
2026-09-06 17:06:40 +0100

    Conserei o header

--- Projetos/Applications/TaskFlow/js/main.js
+++ Projetos/Applications/TaskFlow/js/main.js
@@ -7,27 +7,51 @@ const listaTarefas = document.querySelector(".list-tasks")
 const filtros = document.querySelectorAll(".btn-filters")
 
 
+// ==================== MENU ====================
+
+const btnMenu = document.querySelector("#btn-menu");
+const menu = document.querySelector("#menu");
+const iconMenu = btnMenu.querySelector("i");
+
+
+btnMenu.addEventListener("click", () => {
+    menu.classList.toggle("ativo");
+
+    iconMenu.classList.toggle("fa-bars");
+    iconMenu.classList.toggle("fa-xmark");
+
+    overlay.classList.toggle("ativo", menu.classList.contains("ativo"));
+});
+
+
 // ==================== MODAL ====================
 
 btnCancel.forEach(btnCancel => {
     btnCancel.addEventListener("click", () => {
-        modal.style.display = "none"
-        overlay.classList.remove("ativo")
-    })
-})
+        modal.style.display = "none";
+        overlay.classList.remove("ativo");
+    });
+});
 
 btnAdd.forEach(btnAdd => {
     btnAdd.addEventListener("click", () => {
-        modal.style.display = "flex"
-        overlay.classList.add("ativo")
-    })
-})
+        modal.style.display = "flex";
+        overlay.classList.add("ativo");
+    });
+});
+
+
+// ==================== OVERLAY ====================
 
 overlay.addEventListener("click", () => {
-    modal.style.display = "none"
-    overlay.classList.remove("ativo")
-})
+    modal.style.display = "none";
+    menu.classList.remove("ativo");
 
+    iconMenu.classList.remove("fa-xmark");
+    iconMenu.classList.add("fa-bars");
+
+    overlay.classList.remove("ativo");
+});
 
 // ==================== PESQUISA ====================
 
